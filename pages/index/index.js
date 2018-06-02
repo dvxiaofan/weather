0688747a0d962e4da3b28987dccec8e3d15b12aa
xiaofan <1353815633@qq.com>
2018-06-03 01:07:02 +0800

    动态获取天气数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,32 @@
-Page({})
+
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+Page({
+	onLoad() {
+		wx.request({
+			url: 'https://test-miniprogram.com/api/weather/now',
+			data: {
+				city: 'guangzhou'
+			},
+			method: 'GET',
+			success: res => {
+				let result = res.data.result;
+				let temp = result.now.temp;
+				let weather = result.now.weather;
+				
+				this.setData({
+					nowTemp: `${temp}°`,
+					nowWeather: weatherMap[weather]
+				})
+			}
+		})
+	}
+
+})
